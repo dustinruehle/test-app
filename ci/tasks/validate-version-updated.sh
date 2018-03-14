7fe82fce871be6a5ae4a1de1ca2f8807c8de57cf
dustinruehle <druehle@pivotal.io>
2018-03-14 05:22:00 -0400

    updating

--- ci/tasks/validate-version-updated.sh
+++ ci/tasks/validate-version-updated.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
-echo "Hello again!"
-ls -al
-sleep 10
+set -e
+
+cf api $CF_API --skip-ssl-validation
+
+cf login -u $CF_USER -p $CF_PASSWORD -o "$CF_ORG" -s "$CF_SPACE"
+
+set -x
+set +e
+
+VERSION=$(cat version/version)
+
+cf apps | grep "alarmd" | awk '{ print $1; }' | sed -n -e 's/^.*-//p' | grep $VERSION
+
+if [ $? -eq 0 ]
+then
+  echo "ERROR: alarmd version already deployed!"
+  exit 1
+
+else
+  echo "alarmd not found"
+fi
